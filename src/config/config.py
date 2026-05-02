c8e79c15fa5f9ea559ab65219d3c4040a80b4e38
Jeevant Prakhar Singh <anupamasinghsrinet1976@gmail.com>
2026-05-02 11:58:09 +0530

    Added: configuration files and logger and custom exception files

---
+++ src/config/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from src.utils.logger import get_logger
+
+load_dotenv()
+
+logger= get_logger(__name__)
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+#SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+
+logger.info("Configuration loaded successfully !")
